dcd27f03b0c3721d4783db5e04f321d83a1455b4
Juan Pablo Maldonado Castro <maldonadocastrojp@gmail.com>
2023-09-22 17:09:23 -0600

    Error handling for single item id not found

--- APIs/LittleLemon/LittleLemonAPI/views.py
+++ APIs/LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from . import models, serializers
@@ -12,6 +13,6 @@ def menu_items(request):
 
 @api_view()
 def single_item(request,id):
-    item = models.MenuItem.objects.get(pk=id)
+    item =get_object_or_404(models.MenuItem,pk=id)
     serialized_item = serializers.MenuItemSerializer(item)
     return Response(serialized_item.data)
