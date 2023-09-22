57d12c65a2223f3a6684d535197f132ee57281c0
Juan Pablo Maldonado Castro <maldonadocastrojp@gmail.com>
2023-09-22 17:08:05 -0600

    Implemented serializers

--- APIs/LittleLemon/LittleLemonAPI/views.py
+++ APIs/LittleLemon/LittleLemonAPI/views.py
@@ -1,8 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from . import models
+from . import models, serializers
 
 @api_view()
 def menu_items(request):
     items = models.MenuItem.objects.all()
-    return Response(items.values())
+    serialized_item = serializers.MenuItemSerializer(items, many=True)
+
+    return Response(serialized_item.data)
+
+
+@api_view()
+def single_item(request,id):
+    item = models.MenuItem.objects.get(pk=id)
+    serialized_item = serializers.MenuItemSerializer(item)
+    return Response(serialized_item.data)
