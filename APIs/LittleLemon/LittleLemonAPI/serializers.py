186cb6c34b23ab0ea7e98877eb4465b1bc99db9e
Juan Pablo Maldonado Castro <maldonadocastrojp@gmail.com>
2023-09-22 17:16:11 -0600

    Updated serializer for changing variable name and adding method based results

--- APIs/LittleLemon/LittleLemonAPI/serializers.py
+++ APIs/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,6 +1,6 @@
+from decimal import Decimal
 from rest_framework import serializers
 from . import models
-
 #class MenuItemSerializer(serializers.Serializer):
 #    id = serializers.IntegerField()
 #    title = serializers.CharField(max_length=255)
@@ -8,6 +8,12 @@ from . import models
 #    inventory = serializers.IntegerField()
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    # Change name of variable in API: inventory -> stock
+    stock = serializers.IntegerField(source="inventory")
+    price_after_tax = serializers.SerializerMethodField(method_name="calculate_tax")
     class Meta:
         model = models.MenuItem
-        fields = ["id", "title", "price", "inventory"]
+        fields = ["id", "title", "price", "stock", "price_after_tax"]
+
+    def calculate_tax(self, product:models.MenuItem):
+        return product.price * Decimal(1.1)
