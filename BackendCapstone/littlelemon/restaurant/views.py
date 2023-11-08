db38a45416db0f2322678e730ec15904973527ee
Juan Pablo Maldonado Castro <maldonadocastrojp@gmail.com>
2023-11-08 14:43:33 -0600

    Setup API

--- BackendCapstone/littlelemon/restaurant/views.py
+++ BackendCapstone/littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . import models, serializers
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = models.Booking.objects.all()
+    serializer_class = serializers.BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
