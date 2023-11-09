41e6ab4e8f83217548d2c4ee054830120cd4e8d3
Juan Pablo Maldonado Castro <maldonadocastrojp@gmail.com>
2023-11-09 13:55:51 -0600

    Removed unnecesary import

--- BackendCapstone/littlelemon/restaurant/views.py
+++ BackendCapstone/littlelemon/restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import authtoken, generics, viewsets, permissions
+from rest_framework import generics, viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from . import models, serializers
